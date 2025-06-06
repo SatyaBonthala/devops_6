f07e81878fc560fffc09eadc31cc6501acfae1e7
SatyaBonthala <satyabonthala7@gmail.com>
2025-06-06 16:35:54 +0530

    commit

---
+++ src/main/java/prg11demo/prg11demo/App.java
@@ -0,0 +1,13 @@
+package prg11demo.prg11demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
